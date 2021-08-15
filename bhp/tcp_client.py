56c8b2585f160153472f5e82efefc3b662b36bb1
TabaKhan <qelichkhan@gmail.com>
2021-08-15 09:39:27 +0000

    FIX handler accepts and handles connection

--- bhp/tcp_client.py
+++ bhp/tcp_client.py
@@ -1,13 +1,13 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "23.88.63.176"
+target_port = 4364
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-client.send(b"GET / HTTP/1.1\r\nHost:google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHost:23.88.63.176\r\n\r\n")
 
 response = client.recv(4096)
 print(response.decode())
--- bhp/tcp_server.py
+++ bhp/tcp_server.py
@@ -4,6 +4,12 @@ import threading
 IP = '0.0.0.0'
 PORT = 9999
 
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Received:{request.decode("utf-8")}')
+        sock.send(b'ACK') 
+
 def main():
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server.bind((IP,PORT))
@@ -15,13 +21,7 @@ def main():
         print(f'[*] Accepted connection from {address[0]}:{address[1]}')
         
         client_handler = threading.Thread(target= handle_client, args=(client,))
-        client_handler.start()
-
-        def handle_client(client_socket):
-            with client_socket as sock:
-                request = sock.recv(1024)
-                print(f'[*] Received:{request.decode("utf-8")}')
-                client.send(b'ACK')        
+        client_handler.start()       
 
 if __name__ == '__main__':
     main()
